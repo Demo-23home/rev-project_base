f6fc3b13023d2ba46ebc7e513e1b133e94704033
Demo-23home <zeyadslama23@gmail.com>
2025-06-16 15:17:43 +0300

    Adding Logging, Configuring Postgresql

--- api/config/settings/local.py
+++ api/config/settings/local.py
@@ -1,13 +1,13 @@
 from os import path, getenv
 from dotenv import load_dotenv
 
-from .base import * #noqa
+from .base import *  # noqa
 
 from .base import BASE_DIR
 
 local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
 
-if path.isfile(local_env_file): 
+if path.isfile(local_env_file):
     load_dotenv(local_env_file)
 
 DEBUG = True
@@ -22,12 +22,28 @@ EMAIL_BACKEND = "djcelery.email.backends.CeleryEmailBackend"
 
 EMAIL_HOST = getenv("EMAIL_HOST")
 
-EMAIL_PORT = getenv("EMAIL_PORT") 
+EMAIL_PORT = getenv("EMAIL_PORT")
 
 DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
 
 DOMAIN = getenv("DOMAIN")
 
-ALLOWED_HOSTS = ["0.0.0.0", "local_host", "127.0.0.1"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
 
-
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(name)-12s %(asctime)s %(module)s %(process)d %(thread)d  %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
