7d3912e01045cf0e2a8cf917ef8079c0fb245268
Demo-23home <zeyadslama23@gmail.com>
2025-06-17 18:21:33 +0300

    Adding User(creation, change) forms

--- api/core_apps/users/admin.py
+++ api/core_apps/users/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = ["pkid", "id", "username", "email", "first_name", "last_name"]
+    list_display_links = ["pkid", "id", "username", "email"]
+    ordering = ["pkid"]
+    search_fields = ["username", "email", "first_name", "last_name"]
+    fieldsets = (
+        (_("Login Credentials"), {"fields": ("email", "password")}),
+        (_("Personal Info"), {"fields": ("first_name", "last_name", "username")}),
+        (
+            _("Permissions & Groups"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important Dates"), {"fields": ("date_joined", "last_login", "username")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "username",
+                    "first_name",
+                    "last_name",
+                ),
+            },
+        ),
+    )
